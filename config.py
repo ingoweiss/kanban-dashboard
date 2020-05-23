45d63646e994ba127f03e544aa5a3d25b6ec2141
Ingo Weiss <ingo@ingoweiss.com>
2020-05-23 15:59:33 -0400

    add structuring to config

--- config.py
+++ config.py
@@ -8,4 +8,6 @@ class Config:
     
     project_start_date = pd.to_datetime(_data['project_start_date']).date()
     project_duration = _data['project_duration']
+    structuring = _data['structuring']
+    structuring_factor = 1 + structuring
 
