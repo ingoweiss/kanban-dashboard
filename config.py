9bd70fca21e4fe2cf35633aa8fb6a0ade5e0fb87
Ingo Weiss <ingo@ingoweiss.com>
2020-05-31 12:43:51 -0400

    refactor Config to use singleton pattern

--- config.py
+++ config.py
@@ -1,19 +1,29 @@
 import json
 import pandas as pd
 from pandas.tseries.holiday import USFederalHolidayCalendar
+import pdb
 
 class Config:
 
-    with open('data/config.json') as json_file:
-        _data = json.load(json_file)
-    
-    calendar = USFederalHolidayCalendar()
-    project_start_date = pd.to_datetime(_data['project_start_date'])
-    project_duration = _data['project_duration']
-    project_end_date = project_start_date + pd.Timedelta(days=project_duration)
-    project_dates = pd.date_range(project_start_date, project_end_date, freq='D')
-    project_working_dates = pd.date_range(project_start_date, project_end_date, freq=pd.offsets.CDay(calendar=calendar))
-    structuring = _data['structuring']
-    structuring_factor = 1 + structuring
+    _instance = None
+
+    def __init__(self):
+        with open('data/config.json') as json_file:
+            self._data = json.load(json_file)
+        self.calendar = USFederalHolidayCalendar()
+        self.project_start_date = pd.to_datetime(self._data['project_start_date'])
+        self.project_duration = self._data['project_duration']
+        self.project_end_date = self.project_start_date + pd.Timedelta(days=self.project_duration)
+        self.project_dates = pd.date_range(self.project_start_date, self.project_end_date, freq='D')
+        self.project_working_dates = pd.date_range(self.project_start_date, self.project_end_date, freq=pd.offsets.CDay(calendar=self.calendar))
+        self.project_working_days = [(d - self.project_start_date).days+1 for d in self.project_working_dates]
+        self.structuring = self._data['structuring']
+        self.structuring_factor = 1 + self.structuring
+
+    @classmethod
+    def instance(cls):
+        if not cls._instance:
+            cls._instance = Config()
+        return cls._instance
 
     
--- data.py
+++ data.py
@@ -4,12 +4,14 @@ import numpy as np
 import json
 from datetime import datetime
 
-from config import Config as Conf
+from config import Config
 
 class Data:
 
     @classmethod
     def stories(cls):
+
+        conf = Config.instance()
 
         stories = pd.read_json('data/stories.json', dtype={'size': 'int64', 'start_date': 'datetime64[D]', 'end_date': 'datetime64[D]'})\
                     .rename(columns={'id': 'ID', 'size': 'Size', 'start_date': 'Start Date', 'end_date': 'End Date (Actual)'})\
@@ -19,9 +21,9 @@ class Data:
         completed = stories['End Date (Actual)'].notna()
         started = stories['Start Date'].notna()
 
-        stories['Start Project Day'] = (stories['Start Date'] - Conf.project_start_date).dt.days.astype('Int64')
-        stories['End Project Day (Actual)'] = (stories['End Date (Actual)'] - Conf.project_start_date).dt.days.astype('Int64')
-        stories['Story Days (Estimated)'] = round(stories['Size'] * Conf.structuring_factor).astype('Int64')
+        stories['Start Project Day'] = (stories['Start Date'] - conf.project_start_date).dt.days.astype('Int64')
+        stories['End Project Day (Actual)'] = (stories['End Date (Actual)'] - conf.project_start_date).dt.days.astype('Int64')
+        stories['Story Days (Estimated)'] = round(stories['Size'] * conf.structuring_factor).astype('Int64')
         stories['End Project Day (Estimated)'] = stories['Start Project Day'] + stories['Story Days (Estimated)'] - 1
         stories['End Project Day (Actual or Estimated)'] = stories['End Project Day (Actual)'].combine_first(stories['End Project Day (Estimated)'])
         stories['End Date (Estimated)'] = stories['Start Date'] + pd.to_timedelta(stories['Story Days (Estimated)'], 'd')
@@ -41,17 +43,19 @@ class Data:
     @classmethod
     def story_days(cls):
 
+        conf = Config.instance()
+
         stories = cls.stories()
 
         # determine elapsed days since estimated end for overdue stories:
-        project_day = (pd.to_datetime('today') - Conf.project_start_date).days + 1
+        project_day = (pd.to_datetime('today') - conf.project_start_date).days + 1
         overdue = stories['In-Flight'] & (stories['End Project Day (Estimated)'] < project_day)
         stories.loc[overdue, 'Overdue Days'] = project_day - stories.loc[overdue, 'End Project Day (Estimated)']
         stories['Overdue Days'] = stories['Overdue Days'].astype('Int64') # FIXME this works only if I cast to int in a separate step, otherwise it ends up a float column - why?
 
         # determine and explode story days of all started stories:
         started = stories['Start Date'].notna()
-        project_working_days = [(d - Conf.project_start_date).days +1 for d in Conf.project_working_dates]
+        project_working_days = [(d - conf.project_start_date).days +1 for d in conf.project_working_dates]
         stories.loc[started, 'Project Days'] = stories.apply(lambda s: [d for d in range(s['Start Project Day'], s['End Project Day (Actual or Estimated)']+1) if d in project_working_days], axis=1)
         story_days = stories[started].explode('Project Days').rename(columns={'Project Days': 'Project Day'})
 
