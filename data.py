dbcd9df94b1e22317d09e4dc0a693b79c84dd3e6
Ingo Weiss <ingo@ingoweiss.com>
2020-10-15 17:37:05 -0400

    comment 'stories' method in data.py

--- data.py
+++ data.py
@@ -25,17 +25,39 @@ class Data:
         started = stories['Start Date'].notna()
         in_flight = stories['Start Date'].notna() & stories['End Date (Actual)'].isna()
 
+        # Add estimate of days needed to implement the story, based on size and configured structuring factor:
         stories['Story Days (Estimated)'] = round(stories['Size'] * conf.structuring_factor).astype('Int64')
+
+        # For in-flight stories, add the number of business days since they were started:
         stories.loc[in_flight, 'Story Days (Elapsed)'] = stories.loc[in_flight].apply(lambda s: len(pd.date_range(s['Start Date'], today, freq=conf.offset)), axis=1)
+
+        # For completed stories, add the number of business days used to complete them:
         stories.loc[completed, 'Story Days (Actual)'] = stories.loc[completed].apply(lambda s: len(pd.date_range(s['Start Date'], s['End Date (Actual)'], freq=conf.offset)), axis=1)
+
+        # For in-flight and completed stories, add the estimated end date based on 'Start Date' and 'Story Days (Estimated)':
         stories.loc[started, 'End Date (Estimated)'] = stories.loc[started].apply(lambda s: s['Start Date'] + pd.offsets.CDay(calendar=conf.calendar, n=s['Story Days (Estimated)']-1), axis=1)
+
+        # For completed stories, add the actual cumulative burn-up in terms of story size:
         stories.loc[completed, 'Burn Up (Actual)'] = stories['Size'].expanding().sum()#.astype('Int64')
+
+        # For started stories, add the estimated cumulative burn-up in terms of story size (burn-up if all stories are completed at the estimated date):
         stories.loc[started, 'Burn Up (Estimated)'] = stories['Size'].expanding().sum()#.astype('Int64')
+
+        # Add burn-up, using actual if available, otherwise estimated:
         stories['Burn Up (Actual or Estimated)'] = stories['Burn Up (Actual)'].combine_first(stories['Burn Up (Estimated)'])
+
         total_scope = stories['Size'].values.sum()
+
+        # Add actual burn-down, as the inverse of actual burn-up:
         stories.loc[completed, 'Burn Down (Actual)'] = (total_scope - stories['Burn Up (Actual)'])#.astype('Int64')
+
+        # Add estimated burn-down, as the inverse of estimated burn-up:
         stories.loc[started, 'Burn Down (Estimated)'] = (total_scope - stories['Burn Up (Estimated)'])#.astype('Int64')
+
+        # Add burn-down, using actual if available, otherwise estimated:
         stories['Burn Down (Actual or Estimated)'] = stories['Burn Down (Actual)'].combine_first(stories['Burn Down (Estimated)'])
+
+        # Add 'relative cycle time' as actual story days per estimated story day:
         stories.loc[completed, 'Relative Cycle Time'] = stories['Story Days (Actual)'] / stories['Story Days (Estimated)']
 
         return stories
