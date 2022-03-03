1525a4d15a186f7054fe1de4159530b7f741279c
Ingo Weiss <ingo@ingoweiss.com>
2022-03-03 15:35:58 -0500

    discard time part of today date to fix problem

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ class Config:
         with open(Config._data_dir + '/config.json') as json_file:
             self._data = json.load(json_file)
         if 'today' in self._data.keys():
-            self.today = pd.to_datetime(self._data['today'])
+            self.today = pd.to_datetime(self._data['today']).normalize()
         else:
-            self.today = pd.to_datetime('today')
+            self.today = pd.to_datetime('today').normalize()
         self.calendar = ProjectCalendar()
         self.offset = pd.offsets.CDay(calendar=self.calendar)
         self.forecast_mode = self._data['forecast_mode']
