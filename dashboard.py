dcb39f6357754767579b723fdc8a1777900d9d94
Ingo Weiss <ingo@ingoweiss.com>
2022-03-03 18:27:26 -0500

    get range slider to display dates

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,13 @@
-from datetime import datetime
-
+import datetime
+import pdb
 import dash
 from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
 import numpy as np
 import flask
+from data import Data as Dat
+from config import Config
 
 from graphs import Graphs as Grph
 
@@ -14,13 +16,45 @@ external_stylesheets = [dbc.themes.COSMO]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
+conf = Config.instance()
+stories = Dat.stories(conf.today).reset_index()
+epoch = pd.to_datetime('1970-01-01')
+
+# Dates:
+project_start_date = stories['Start Date'].min()
+project_end_date = stories['End Date (Actual or Current Estimated)'].max()
+project_dates = pd.date_range(project_start_date, project_end_date, freq='D')
+project_working_dates = pd.date_range(project_start_date, project_end_date, freq=conf.offset)
+project_nonworking_dates = [d for d in project_dates if d not in project_working_dates]
+
+monday_before_project_start_date = project_start_date - datetime.timedelta(days=project_start_date.weekday())
+monday_after_project_end_date = project_end_date + datetime.timedelta(days=((7 - project_end_date.weekday())%7))
+
+
+range = pd.date_range(
+    start=monday_before_project_start_date,
+    end=monday_after_project_end_date,
+    freq='W-MON'
+)
+
+formatted_range = {(d-epoch).days:d.strftime("%y-%m-%d") for d in range}
+formatted_range_keys = list(formatted_range.keys())
+# pdb.set_trace()
+
 app.layout = dbc.Container([
     dbc.Row(dbc.Col(html.H1('Kanban Dashboard'), md=12, className='mb-3')),
     dbc.Row([
         dbc.Col(dbc.Card([
             dbc.CardHeader('Timeline'),
             dbc.CardBody([
-                dcc.RangeSlider(0, 20, 1, value=[5, 15], id='my-range-slider'),
+                dcc.RangeSlider(
+                    min=formatted_range_keys[0],
+                    max=formatted_range_keys[-1],
+                    step=None,
+                    marks=formatted_range,
+                    value=[formatted_range_keys[-4], formatted_range_keys[-1]],
+                    id='my-range-slider'
+                ),
                 html.Div(id='output-container-range-slider'),
                 Grph.timeline()
             ])
